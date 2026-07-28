783bfe87ee5060f912cf6fdab2fc6b49fe181cca
Student at UOH <atifshehzad9098@gmial.com>
2026-07-28 20:49:20 +0500

    Initial commit from Create Next App

---
+++ lib/events.tsx
@@ -0,0 +1,20 @@
+const events = [
+{
+id:1,
+title:"React Summit",
+category:"Frontend",
+speaker:"Ali",
+venue:"Lahore",
+rsvp:150
+},
+{
+id:2,
+title:"AI Conference",
+category:"AI",
+speaker:"Ahmed",
+venue:"Islamabad",
+rsvp:220
+}
+]
+
+export default events;
